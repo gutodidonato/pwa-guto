b88055e0c0ae6e0a67ea19d1010ef322e86e3b72
gutodidonato <rm99433@fiap.com.br>
2024-05-06 17:43:09 -0300

    menu acordeon, contato acordeon, ajustes gerais

--- js/index.js
+++ js/index.js
@@ -1,43 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
   let lastScrollPosition = window.scrollY;
+  let timeIsRunning = true;
 
   const body = document.body;
   const header = document.querySelector("header");
   const logo = document.querySelector(".logo");
+  const telaMobile = screen.width < 900;
 
   window.addEventListener("scroll", () => {
     const currentScrollPosition = window.scrollY;
+    const telaSobe = currentScrollPosition < lastScrollPosition;
+    const telaProximaHeader = currentScrollPosition < 500;
 
-    if (
-      currentScrollPosition < lastScrollPosition &&
-      currentScrollPosition > 300 &&
-      screen.width > 900
-    ) {
-      /* Subiu a tela */
+    /* RETORNOS BOLEANOS */
+
+    if (telaSobe && !telaProximaHeader && timeIsRunning && !telaMobile) {
+      /* Subiu a tela lá embaixo */
       header.classList.add("header_slide-in");
       header.classList.remove("header_slide-out");
       /*----------*/
+    } else if (telaProximaHeader && timeIsRunning && !telaMobile) {
+      /*situação de ajuste de header para volta */
+      if (header.classList.contains("header_slide-in")) {
+        header.classList.replace("header_slide-in", "header_slide-out");
+        timeIsRunning = false;
+        setTimeout(() => {
+          header.classList.remove("header_slide-out");
+        }, 250);
+        timeIsRunning = true;
+      }
     } else if (
-      currentScrollPosition > lastScrollPosition &&
-      currentScrollPosition > 300 &&
-      screen.width > 900 &&
+      !telaSobe &&
+      !telaProximaHeader &&
+      timeIsRunning &&
+      !telaMobile &&
       header.classList.contains("header_slide-in")
     ) {
-      /* Animações de transição, descendo a tela */
+      /* tela desce lá embaixo */
       header.classList.remove("header_slide-in");
       header.classList.add("header_slide-out");
-      /*----------*/
-    } else if (currentScrollPosition > 300) {
-      /*situação de ajuste de header para volta */
-      logo.classList.add("logo_header-reduzido");
-      /*------------*/
-    } else {
-      /* Tela lá em cima */
-      header.classList.remove("header_slide-in");
-      header.classList.remove("header_slide-in");
-      /*----------*/
     }
-    /* Passando valor */
-    lastScrollPosition = currentScrollPosition;
+    setTimeout(() => {
+      lastScrollPosition = currentScrollPosition;
+    }, 100);
   });
+
+  const checkbox = document.querySelector("#checkbox-botao-menu");
 });
