c181e4838321947d70b10eca75908d1c0df8fd05
gutodidonato <rm99433@fiap.com.br>
2024-05-07 15:30:29 -0300

    quase full css menu

--- js/index.js
+++ js/index.js
@@ -5,22 +5,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const body = document.body;
   const header = document.querySelector("header");
   const logo = document.querySelector(".logo");
-  const telaMobile = screen.width < 900;
 
   window.addEventListener("scroll", () => {
     const currentScrollPosition = window.scrollY;
     const telaSobe = currentScrollPosition < lastScrollPosition;
     const telaProximaHeader = currentScrollPosition < 500;
+    const telaMobile = screen.width < 900;
 
-    /* RETORNOS BOLEANOS */
+    /* Variaveis */
 
     if (telaSobe && !telaProximaHeader && timeIsRunning && !telaMobile) {
       /* Subiu a tela lá embaixo */
+      /*----------*/
+      /*----------*/
       header.classList.add("header_slide-in");
       header.classList.remove("header_slide-out");
       /*----------*/
+      /*----------*/
     } else if (telaProximaHeader && timeIsRunning && !telaMobile) {
       /*situação de ajuste de header para volta */
+      /*----------*/
+      /*----------*/
       if (header.classList.contains("header_slide-in")) {
         header.classList.replace("header_slide-in", "header_slide-out");
         timeIsRunning = false;
@@ -29,6 +34,8 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 250);
         timeIsRunning = true;
       }
+      /*----------*/
+      /*----------*/
     } else if (
       !telaSobe &&
       !telaProximaHeader &&
@@ -37,8 +44,12 @@ document.addEventListener("DOMContentLoaded", function () {
       header.classList.contains("header_slide-in")
     ) {
       /* tela desce lá embaixo */
+      /*----------*/
+      /*----------*/
       header.classList.remove("header_slide-in");
       header.classList.add("header_slide-out");
+      /*----------*/
+      /*----------*/
     }
     setTimeout(() => {
       lastScrollPosition = currentScrollPosition;
